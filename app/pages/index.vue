7d03126a77e10e9b2cdab9d3fd285fa548376312
Arthur DANJOU <arthurdanjou@outlook.fr>
2024-06-21 13:09:43 +0200

    Add seo description

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,3 +3,11 @@
     <ContentDoc path="/" />
   </main>
 </template>
+
+<script lang="ts" setup>
+const description = 'I\'m Arthur, a Mathematics lover and IA enthusiast. I\'m currently studying at the University of Paris-Saclay. I\'m passionate about Mathematics, Computer Science, and Artificial Intelligence.'
+useSeoMeta({
+  title: 'Arthur Danjou - Mathematics Lover and IA Enthusiast',
+  description
+})
+</script>
