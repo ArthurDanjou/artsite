b5ba2b6774e5486f28c1d83f65843103a3b1df15
barbapapazes <e.soubiran25@gmail.com>
2024-06-23 09:22:35 +0200

    fix: speed up stats

--- server/api/stats.get.ts
+++ server/api/stats.get.ts
@@ -1,13 +1,57 @@
+import { H3Event } from 'h3'
+
 export default defineEventHandler(async (event) => {
-  const config = useRuntimeConfig(event)
-  const coding = await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeCodig}.json`)
-  const editors = await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeEditors}.json`)
-  const os = await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeOs}.json`)
-  const languages = await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeLanguages}.json`)
+  const [coding, editors, os, languages] = await Promise.all([
+    cachedWakatimeCoding(event),
+    cachedWakatimeEditors(event),
+    cachedWakatimeOs(event),
+    cachedWakatimeLanguages(event)
+  ])
+
   return {
     coding,
     editors,
     os,
     languages
   }
+})
+
+const cachedWakatimeCoding = defineCachedFunction(async (event: H3Event) => {
+  const config = useRuntimeConfig(event)
+
+  return await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeCodig}.json`)
+}, {
+  maxAge: 24 * 60 * 60, // Wakatime does not update there data more than once a day
+  name: 'wakatime',
+  getKey: () => 'coding'
+})
+
+const cachedWakatimeEditors = defineCachedFunction(async (event: H3Event) => {
+  const config = useRuntimeConfig(event)
+
+  return await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeEditors}.json`)
+}, {
+  maxAge: 24 * 60 * 60, // Wakatime does not update there data more than once a day
+  name: 'wakatime',
+  getKey: () => 'editors'
+})
+
+const cachedWakatimeOs = defineCachedFunction(async (event: H3Event) => {
+  const config = useRuntimeConfig(event)
+
+  return await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeOs}.json`)
+}, {
+  maxAge: 24 * 60 * 60, // Wakatime does not update there data more than once a day
+  name: 'wakatime',
+  getKey: () => 'os'
+})
+
+const cachedWakatimeLanguages = defineCachedFunction(async (event: H3Event) => {
+  const config = useRuntimeConfig(event)
+
+  return await $fetch(`https://wakatime.com/share/${config.wakatimeUserId}/${config.wakatimeLanguages}.json`)
+}, {
+  maxAge: 24 * 60 * 60, // Wakatime does not update there data more than once a day
+  name: 'wakatime',
+  getKey: () => 'languages'
 })
