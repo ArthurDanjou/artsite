964edfe5604a02f36d6e886b337a412ed2f681ca
Arthur DANJOU <arthurdanjou@outlook.fr>
2024-07-01 01:16:37 +0200

    Fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -34,11 +34,9 @@ export default defineNuxtConfig({
 
   // Nuxt SEO
   site: {
-    url: 'https://artsite.nuxt.dev',
     name: 'Arthur Danjou',
     description: 'I\'m Arthur, a Mathematics lover and IA enthusiast. I\'m currently studying at the University of Paris-Saclay. I\'m passionate about Mathematics, Computer Science, and Artificial Intelligence.',
-    defaultLocale: 'en',
-    trailingSlash: false
+    defaultLocale: 'en'
   },
 
   // Disable to reduce size
