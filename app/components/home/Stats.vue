f29a3ed9ab6c47182d3a5af87d9151e0e08efa39
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-03-19 11:00:32 +0100

    Testing

--- app/components/home/Stats.vue
+++ app/components/home/Stats.vue
@@ -18,9 +18,6 @@ const hours = usePrecision(stats.value!.coding.data.grand_total.total_seconds_in
 <template>
   <ClientOnly>
     <div>{{ stats }}</div>
-    <div>{{ time }}</div>
-    <div>{{ date }}</div>
-    <div>{{ hours }}</div>
     <i18n-t
       v-if="stats && stats.coding && stats.editors && stats.os && stats.languages && time && date && hours"
       keypath="stats"
