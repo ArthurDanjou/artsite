48be5b64bd0bf5f71fd18fff2f0f1b9ea7581fb4
Arthur DANJOU <arthurdanjou@outlook.fr>
2025-03-19 11:21:03 +0100

    Testing 3

--- app/components/home/Stats.vue
+++ app/components/home/Stats.vue
@@ -6,7 +6,7 @@ const { locale, locales } = useI18n()
 const currentLocale = computed(() => locales.value.find(l => l.code === locale.value))
 
 const { data: stats } = await useFetch<Stats>('/api/stats')
-console.log(stats.value)
+console.log(stats)
 const { t } = useI18n({
   useScope: 'local',
 })
