a6d73bf3f48d75c5d64613b153aaa7d69e9df18d
Arthur Danjou <arthurdanjou@outlook.fr>
2025-07-04 13:29:25 +0200

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,11 @@
 <script lang="ts" setup>
 const { locale } = useI18n()
 
+useSeoMeta({
+  title: 'Arthur Danjou - AI enjoyer and Maths student',
+  description: 'Developer enjoying Artificial Intelligence and Machine Learning. Mathematics Student at Paris Dauphine-PSL University specialised in Statistics',
+})
+
 const { data: page } = await useAsyncData(`/home/${locale.value}`, () => {
   return queryCollection('main').path(`/home/${locale.value}`).first()
 }, {
