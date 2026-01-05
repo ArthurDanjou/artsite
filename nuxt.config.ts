b476033a970578dd2da6a4e83d3cda4a1df460e8
Arthur DANJOU <arthurdanjou@outlook.fr>
2026-01-05 16:45:18 +0100

    fix: supprimer la configuration de déploiement pour Cloudflare dans nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -118,10 +118,6 @@ export default defineNuxtConfig({
   nitro: {
     preset: 'cloudflare_module',
 
-    cloudflare: {
-      deployConfig: true
-    },
-
     experimental: {
       openAPI: true
     },
