0703ac7ff73176a0606a7df30e95796fd19b7c4c
Arthur DANJOU <arthurdanjou@outlook.fr>
2026-02-17 09:11:37 +0100

    fix: ajuster la largeur du composant BackgroundTitle pour un meilleur affichage

--- app/components/content/BackgroundTitle.vue
+++ app/components/content/BackgroundTitle.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-  <h1 class="mb-2 font-bold text-4xl md:text-7xl text-transparent opacity-15 text-stroke-neutral-400 dark:text-stroke-neutral-500 text-stroke-2 md:-translate-x-16">
+  <h1 class="w-full md:w-[110%] mb-2 font-bold text-4xl md:text-7xl text-transparent opacity-15 text-stroke-neutral-500 text-stroke-2 md:-translate-x-16">
     {{ title }}
   </h1>
 </template>
