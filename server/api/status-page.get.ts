266fc63482529d4f3d7841de098e6383d603c10e
Arthur DANJOU <arthurdanjou@outlook.fr>
2026-03-11 10:40:00 +0100

    fix: supprimer le débogage de l'environnement et ajouter un journal pour la récupération des données de la page d'état

--- server/api/status-page.get.ts
+++ server/api/status-page.get.ts
@@ -1,5 +1,6 @@
 export default defineCachedEventHandler(async (event) => {
   const { statusPage } = useRuntimeConfig(event)
+  console.log('Fetching status page data...', statusPage)
   return await $fetch(statusPage)
 }, {
   maxAge: 60,
